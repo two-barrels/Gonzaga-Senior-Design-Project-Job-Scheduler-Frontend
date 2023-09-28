6866723f6b361836a7212e8799aef2da29ae37b1
drange <darianvaughnrange@gmail.com>
2023-09-28 13:40:51 -0700

    made some adjustments to http-helper

--- src/services/http-helper.js
+++ src/services/http-helper.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const BASE_URL = 'http://127.0.0.1:3000/'
 
 export default {
-    async get(route, params) {
+    get(route, params) {
         return axios.get(BASE_URL + route, params)
     },
     post(route, params) {
