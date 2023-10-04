8134fc839daaeea525c3f46ab843c552f1130317
cristal104 <cristalmeza104@gmail.com>
2023-10-04 16:03:07 -0700

    finished code for ticket - ready for pr

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <AvailableSpaces />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AvailableSpaces from './components/AvailableSpaces.vue';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AvailableSpaces
+  },
+  data(){
+    return {
+      
+    }
+  },
+  computed: {
+
+  },
+  methods: {
   }
 }
 </script>
 
 <style>
-#app {
+h1 {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
   margin-top: 60px;
+}
+p{
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
 }
 </style>
---
+++ src/components/AvailableSpaces.vue
@@ -0,0 +1,38 @@
+<template>
+    <h1>Available Spaces</h1>
+    <FloorSpace floorName="Floor 1"/>
+    <FloorSpace floorName="Floor 2"/>
+  </template>
+  
+  <script>
+  import FloorSpace from "./FloorSpace.vue"
+  
+  export default {
+    name: 'SingleFloor',
+    props: {
+
+    },
+    components: {
+      FloorSpace
+    }
+  }
+  </script>
+  
+  <style>
+  h1 {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: left;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  p{
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: left;
+    color: #2c3e50;
+  }
+  </style>
+  
