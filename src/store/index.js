8e97947f1ccba111fe35c9e51599272d1c8658b4
cristal104 <cristalmeza104@gmail.com>
2023-10-24 14:01:35 -0700

    added files for http request

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import { createStore } from 'vuex'
+import floor_space from "@/store/modules/floor_space"
+
+
+export default createStore({
+  state: {},
+  mutations: {},
+  actions: {},
+  modules: {
+   floor_space,
+  },
+});
---
+++ src/store/modules/floor_space.js
@@ -0,0 +1,40 @@
+import axios from "axios"
+
+const BASE_URL = 'http://127.0.0.1:3000/'
+
+const state = {
+  spaces_arr: null
+};
+
+const getters = {
+  getSpaceInfo(state) {
+    return state.spaces_arr;
+  }
+};
+
+const actions = {
+    fetchSpaceInfo({commit}) {
+        new Promise((reject) => {
+          axios
+            .get(`${BASE_URL}spaces`)
+            .then((response) => {
+              commit("setSpacesInfo", response);
+            })
+            .catch((error) => {
+              reject(error);
+            });
+        });
+      },
+};
+const mutations = {
+  setSpacesInfo(state, data){
+    state.spaces_arr = data.data
+  }
+};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations,
+};
