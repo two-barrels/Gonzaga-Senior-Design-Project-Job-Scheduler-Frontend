bf7c6f3f70492911eb691d8dc93b139540abd70a
cristal104 <cristalmeza104@gmail.com>
2023-11-12 12:40:33 -0800

    finished ticket

--- src/components/FloorSpace.vue
+++ src/components/FloorSpace.vue
@@ -1,17 +1,31 @@
 <template>
-    <div class = "floor">
-      <vue-collapsible-panel-group>
-    <vue-collapsible-panel :expanded="false">
-        <template #title>
-            {{floorName}}
-        </template>
-        <template #content>
-          <p> Click on a room or desk to check time availability</p>
-          <button> Desk 1</button>
-          <button> Conference Room 1</button>
-        </template>
-    </vue-collapsible-panel>
-</vue-collapsible-panel-group>
+    <div class = "floor" >
+      <vue-collapsible-panel-group >
+        <div>
+          
+          <vue-collapsible-panel :expanded="false" @Click="onGetInfo" v-for="(val, idx) in floors_data" :key="idx">
+              <template #title> 
+                <div class="displayFloors">
+                  Floor {{ val.floor_id }} 
+                </div>
+              </template>
+              <template #content>
+                <p> Click on a room or desk to check time availability:</p>
+                <hr> 
+                <div class="spacesButtons" v-for="(value, index) in spaces_data" :key="index">
+                  <div v-if="val.floor_id == value.floor_id">
+                    <div>
+                      <button>{{value.spaces_name }}</button>  
+                      <p>Space Description: {{ value.description }}</p>
+                      <p>Max Occupancy: {{ value.max_occupancy }}</p>
+                      <hr> 
+                    </div>
+                  </div>
+                </div>
+              </template>
+          </vue-collapsible-panel>             
+        </div>
+      </vue-collapsible-panel-group>
     </div>
   </template>
   
@@ -21,15 +35,52 @@
     VueCollapsiblePanel,
   } from '@dafcoe/vue-collapsible-panel'
   import '@dafcoe/vue-collapsible-panel/dist/vue-collapsible-panel.css'
+  import http_helper from '@/services/http_helper'
+  import "@/store/index.js"
 
   export default {
     name: 'floor-space',
     props: {
       floorName: String,
+      floorNum: Number
     }, 
     components: {
       VueCollapsiblePanelGroup,
       VueCollapsiblePanel,
+    },
+    data(){
+      return{
+        spaces_data: [],
+        floors_data: [],
+        floor_numbers: []
+      }
+    },
+    async mounted(){
+      const spacesPromise = http_helper.get('spaces') 
+      const floorsPromise = http_helper.get('spaces/get_floors')
+      const [spacesResponse,floorsResponse] = await Promise.all([spacesPromise, floorsPromise])
+
+      if(spacesResponse.error) {
+        throw Error
+      }
+      else {
+        this.spaces_data = spacesResponse.data
+      }
+      if(floorsResponse.error){
+        throw Error
+      }
+      else {
+        this.floors_data = floorsResponse.data
+      }
+    },
+    computed: {
+    },
+    methods: {
+      onGetInfo(){
+        console.log("drop-down clicked")
+        console.log(this.spaces_data)
+        console.log(this.floors_data)
+      }
     }
   }
   </script>
@@ -54,5 +105,9 @@
       color: white;
     }
   }
+
+  .hide {
+    visibility: hidden !important;
+}
   </style>
   
