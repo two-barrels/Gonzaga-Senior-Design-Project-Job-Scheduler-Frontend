88cb87d249555cbf0bf4b9993673603203c283a7
EFigueroa12 <efigueroa6912@gmail.com>
2024-01-04 18:24:07 -0800

    make a component for popup, needs debugging and css

---
+++ src/components/SpacePopup.vue
@@ -0,0 +1,67 @@
+<template>
+    <h3> Edit Space </h3>
+        <form>
+          <label for="sname">Space Name:</label><br>
+          <input type="text" class="sname" v-model="spaceDataHold.spaces_name"><br>
+          <label for="floor_num">Floor Name:</label><br>
+          <select name="Floors">
+          <option v-for="(val, idx) in floors_data" :key="idx" @click="changeFloor(val.floor_id)"> {{val.floor_id}}</option> 
+          </select><br>
+          <label for="max_num">Max Occupancy:</label><br>
+          <input type="number" class="max_num" min="1" v-model="spaceDataHold.max_occupancy"><br>
+          <label>Space Details:</label><br>
+          <textarea class="descript" rows="7" cols="50" v-model="spaceDataHold.description"></textarea><bbr></bbr>
+          <div v-if="check">
+            <Button @click="saveChanges()" class ="editbuttsave" label = "Save" > Save </Button>
+            <button @click="closePopup()"> Exit </button> 
+          </div>
+          <div v-else>
+            <Button @click="createSpace();createSpacePopUp()" class ="exitbuttsave" label = "Create Space" ></Button>
+            <button @click="createSpacePopUp()"> Exit </button> 
+          </div>
+        </form>
+</template>
+<script>
+export default {
+    name: 'space-popup',
+    props:{
+        spaceData: Object,
+        floorName: String,
+        floorData:Array,
+        check: Boolean
+    },
+    data(){
+        return{
+            spaceDataHold: {}
+        }
+    },
+    methods:{
+        getCurrSpaceData(){
+            this.spaceDataHold = this.spaceData
+        },
+        changeFloor(floorVal){
+          console.log(floorVal)
+          this.spaceDataHold.floor_id = floorVal
+        },
+        saveChanges() {
+        // Emit an event to notify the parent component about the changes
+        this.$emit('save-changes', this.spaceData);
+        // Close the popup or perform other actions as needed
+        this.closePopup();
+        },
+        createSpaceAndPopup() {
+        // Emit an event to notify the parent component about the new space creation
+        this.$emit('create-space', this.spaceData);
+        // Close the popup or perform other actions as needed
+        this.closePopup();
+        },
+        closePopup() {
+        // Close the popup or perform other actions as needed
+        this.$emit('close-popup');
+        }
+    }
+}
+</script>
+<style lang="scss">
+
+</style>
