175683326dd2e66598640fa971b068769775ac5b
cristal104 <cristalmeza104@gmail.com>
2024-02-17 15:50:19 -0800

    added GET request for users

--- src/components/UserManagement.vue
+++ src/components/UserManagement.vue
@@ -1,29 +1,38 @@
 <template>
 <div class="row">
   <div class="col-4">
-    <div class="list-group overflow-auto" style="max-height: 100vh;" id="list-tab" role="tablist">
-      <a class="list-group-item list-group-item-action active" id="list-home-list" data-bs-toggle="list" href="#list-home" role="tab" aria-controls="list-home">Home</a>
-      <a class="list-group-item list-group-item-action" id="list-profile-list" data-bs-toggle="list" href="#list-profile" role="tab" aria-controls="list-profile">Profile</a>
-      <a class="list-group-item list-group-item-action" id="list-messages-list" data-bs-toggle="list" href="#list-messages" role="tab" aria-controls="list-messages">Messages</a>
-      <a class="list-group-item list-group-item-action" id="list-settings-list" data-bs-toggle="list" href="#list-settings" role="tab" aria-controls="list-settings">Settings</a>
+    <div class="list-group overflow-auto" style="max-height: 100vh;" id="list-tab" role="tablist" v-for="(user,idx) in users_data" :key="idx">
+      <a class="list-group-item list-group-item-action active" id="list-home-list" data-bs-toggle="list" href="#list-home" role="tab" aria-controls="list-home">{{ user.name}}</a>
     </div>
   </div>
   <div class="col-8">
-    <div class="tab-content" id="nav-tabContent">
+    <div class="tab-content" id="nav-tabContent" v-for="(user,idx) in users_data" :key="idx">
       <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">...</div>
-      <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">...</div>
-      <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">...</div>
-      <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">...</div>
     </div>
   </div>
 </div>
   </template>
   
   <script>
-  
+  import Users from '@/services/users-service'
+  import "@/store/index.js"
+
   export default {
     name: 'user-management',
     components: {
+    },
+    data(){
+      return{
+        users_data: []
+      }
+    },
+    async mounted(){
+      try{
+        const usersResponse = await Users.get('users')
+        this.users_data = usersResponse.data
+      } catch (error){
+          console.error(error)
+      }
     }
   }
   </script>
