9d11c45311759b0bcbc6f8d02dca8d90608ad808
cristal104 <cristalmeza104@gmail.com>
2024-02-18 17:04:40 -0800

    fixed user list tabs issue

--- src/components/UserManagement.vue
+++ src/components/UserManagement.vue
@@ -1,16 +1,38 @@
 <template>
-<div class="row">
-  <div class="col-4">
-    <div class="list-group overflow-auto" style="max-height: 100vh;" id="list-tab" role="tablist" v-for="(user,idx) in users_data" :key="idx">
-      <a class="list-group-item list-group-item-action active" id="list-home-list" data-bs-toggle="list" href="#list-home" role="tab" aria-controls="list-home">{{ user.name}}</a>
+  <div class="row">
+      <div class="col-4">
+        <div 
+          class="list-group overflow-auto" 
+          style="max-height: 100vh;" 
+          id="list-tab" 
+          role="tablist"
+        >
+          <a 
+            v-for="(user, idx) in users_data" 
+            :key="idx" 
+            class="list-group-item list-group-item-action" 
+            :id="'list-' + user.name" 
+            data-bs-toggle="list" 
+            role="tab" 
+            :aria-controls="'list-' + user.name" 
+            :href="'#' + user.name">{{ user.name }}
+          </a>
+        </div>
+      </div>
+      <div class="col-8">
+        <div class="tab-content" id="nav-tabContent">
+          <div 
+            v-for="(user, idx) in users_data" 
+            :key="idx" class="tab-pane fade" 
+            :id="user.name" 
+            role="tabpanel" 
+            :aria-labelledby="'list-' + user.name"
+          >
+            <user-permission :user-name="user.name" :user-id="user.id"/>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-  <div class="col-8">
-    <div class="tab-content" id="nav-tabContent" v-for="(user,idx) in users_data" :key="idx">
-      <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list"><user-permission :user-name="user.name"/></div>
-    </div>
-  </div>
-</div>
   </template>
   
   <script>
