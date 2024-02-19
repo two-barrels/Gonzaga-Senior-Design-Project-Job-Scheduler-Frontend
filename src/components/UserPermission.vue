f1d947541c8531b27a63efc52e21eb0b229626bf
cristal104 <cristalmeza104@gmail.com>
2024-02-18 17:34:40 -0800

    finished outline for user permission tabs

--- src/components/UserPermission.vue
+++ src/components/UserPermission.vue
@@ -1,40 +1,69 @@
 <template>
-    <h2>{{ userName }}'s Role: </h2>
-    <h3>Current Access to buildings/floors:</h3>
-    <p>Select dropdown on buildings to view {{ userName }}'s access</p>
+  <div>
+    <h2>{{ userName }}'s Role: </h2> 
+    <std-button 
+      title="Change Role"
+      buttonType="primary-default"
+    />
+  </div>
+  <br>
+  <h3>Current Access to buildings/floors</h3>
+  <p>Select building to view {{ userName }}'s access to floors and spaces</p>
 
-    <div class="row">
-    <div class="col-4">
-        <div class="list-group" id="list-tab" role="tablist">
-        <a class="list-group-item list-group-item-action active" id="list-home-list" data-bs-toggle="list" href="#list-1" role="tab" aria-controls="list-home">Building 1</a>
-        <a class="list-group-item list-group-item-action" id="list-profile-list" data-bs-toggle="list" href="#list-2" role="tab" aria-controls="list-profile">Building 2</a>
-        <a class="list-group-item list-group-item-action" id="list-messages-list" data-bs-toggle="list" href="#list-messages" role="tab" aria-controls="list-messages">Building 3</a>
-        <a class="list-group-item list-group-item-action" id="list-settings-list" data-bs-toggle="list" href="#list-settings" role="tab" aria-controls="list-settings">Building 4</a>
-        </div>
-  </div>
-  <div class="col-8">
-    <div class="tab-content" id="nav-tabContent">
-      <div class="tab-pane fade show active" id="list-1" role="tabpanel" aria-labelledby="list-home-list">...</div>
-      <div class="tab-pane fade" id="list-2" role="tabpanel" aria-labelledby="list-profile-list">...</div>
-      <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">...</div>
-      <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">...</div>
-    </div>
-  </div>
-</div>
+  <vue-collapsible-panel-group class="drop-down">
+    <vue-collapsible-panel
+      :expanded="false"
+    >
+    <template #title>
+      Building 1
+    </template>
+    <template #content>
+      (list floors and spaces here for Building 1)
+    </template>
+    </vue-collapsible-panel>
+    <vue-collapsible-panel
+      :expanded="false"
+    >
+      <template #title>
+        Building 2
+      </template>
+      <template #content>
+        (list floors and spaces for Building 2)
+      </template>
+    </vue-collapsible-panel>
+  </vue-collapsible-panel-group>
+
+  <br>
+  <std-button 
+      title="Edit Building and Floor Access"
+      buttonType="primary-default"
+    />
 </template>
 <script>
-    export default {
+  import StdButton from "@/components/StdButton.vue"
+  import {
+    VueCollapsiblePanelGroup,
+    VueCollapsiblePanel,
+  } from '@dafcoe/vue-collapsible-panel'
+  import '@dafcoe/vue-collapsible-panel/dist/vue-collapsible-panel.css'
+
+  export default {
     name: 'user-permission',
     components: {
-
+      VueCollapsiblePanelGroup,
+      VueCollapsiblePanel,
+      'std-button':StdButton
     },
     props: {
-        userName: String
+      userName: String,
+      userId: String
     },
-    }
-    </script>
-    
-    <style lang="scss" scoped>
-
-    </style>
+  }
+  </script>
+  
+<style lang="scss" scoped>
+.drop-down{
+  width: 33%;
+}
+</style>
       
