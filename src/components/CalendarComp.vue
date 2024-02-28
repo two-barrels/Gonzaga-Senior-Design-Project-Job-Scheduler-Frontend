b7d567889b4d208fea4b833a0360b4b2fe7fd3f6
lnguyen24 <lnguyen5670@gmail.com>
2024-02-27 17:25:54 -0800

    edit/move reservations

--- src/components/CalendarComp.vue
+++ src/components/CalendarComp.vue
@@ -16,7 +16,6 @@
 import {DayPilot, DayPilotCalendar, DayPilotNavigator} from '@daypilot/daypilot-lite-vue'
 import http from '@/services/http-helper.js'
 import date from '@/services/curr-date-helper.js'
-// import session from'@/SessionManager.vue'
 
 export default {
   name: 'CalendarComp',
@@ -39,20 +38,26 @@ export default {
         {
           text: "Show event ID",
           onClick: events => {
-            console.log(events.source.data)
+            console.log(events.source.data["start"]["value"][11] + events.source.data["start"]["value"][12])
           }
         },
         {
           text: "Delete",
           onClick: events => {
-            http.delete(
-              `reservations/${events.source.data["id"]}`, {
-                space_id: this.space_id, 
-                user_id: 101, 
-                start_time: events.source.data["start"], 
-                end_time: events.source.data["end"]
-              })
-            location.reload()
+            try {
+              http.delete(
+                `reservations/${events.source.data["id"]}`, {
+                  space_id: this.space_id, 
+                  user_id: 101, 
+                  start_time: events.source.data["start"], 
+                  end_time: events.source.data["end"]
+                }
+              )
+              this.events = this.events.filter(event => event.id !== events.source.data["id"]);
+              this.calendar.update({ events: this.events });
+            } catch (error) {
+              console.log("Error deleting reservation:", error.message)
+            }
           }
         }
         ]),
@@ -60,12 +65,34 @@ export default {
         durationBarVisible: false,
         timeRangeSelectedHandling: "Enabled",
         onTimeRangeSelected: async (args) => {
+          //console.log(this.events)
+          //DayPilot.Modal.alert("Cannot do this")
+          // const start_time = parseInt(args.start["value"][11] + args.start["value"][12])
+          // const end_time = parseInt(args.end["value"][11] + args.end["value"][12])
+
+          // let existing_start = 0
+          // let existing_end = 0
+          // this.events.forEach(async (item) => {
+          //   existing_start = parseInt(item.start["value"][11] + item.start["value"][12])
+          //   existing_end = parseInt(item.end["value"][11] + item.end["value"][12])
+          //   if (start_time >= existing_start && end_time <= existing_end) {
+          //     args.preventDefault()
+          //     await DayPilot.Modal.alert("Cannot do this")
+          //     return
+          //   }
+          // })
+
+
+
+
           const modal = await DayPilot.Modal.prompt("Create a new event:", "Booked");
           const dp = args.control;
           dp.clearSelection();
           if (modal.canceled) {
             return
           }
+          //if (parseInt(args.start["value"][11] + args.start["value"][12]))
+          //const start_time = 
           dp.events.add({
             start: args.start,
             end: args.end,
@@ -87,19 +114,106 @@ export default {
         },
         eventDeleteHandling: "Disabled",
         eventRightClickHandling: "ContextMenu",
+        eventResizeHandling: "Update",
         onEventMoved: () => {
           console.log("Event moved")
+          this.config.contextMenu = new DayPilot.Menu([
+          {
+            text: "Confirm move",
+            onClick: events => {
+              try {
+                http.put(
+                  `reservations/${events.source.data["id"]}`, {
+                    space_id: this.space_id, 
+                    user_id: 101, 
+                    start_time: events.source.data["start"], 
+                    end_time: events.source.data["end"]
+                  }
+                )
+              } catch (error) {
+                console.log("Error moving reservation:", error.message)
+              }
+              //reset context menu
+              this.config.contextMenu = new DayPilot.Menu([
+              {
+                text: "Show event ID",
+                onClick: events => {
+                  console.log(events.source.data)
+                }
+              },
+              {
+                text: "Delete",
+                onClick: events => {
+                  try {
+                    http.delete(
+                      `reservations/${events.source.data["id"]}`, {
+                        space_id: this.space_id, 
+                        user_id: 101, 
+                        start_time: events.source.data["start"], 
+                        end_time: events.source.data["end"]
+                      }
+                    )
+                    this.events = this.events.filter(event => event.id !== events.source.data["id"]);
+                    this.calendar.update({ events: this.events });
+                  } catch (error) {
+                    console.log("Error deleting reservation:", error.message)
+                  }
+                }
+              }
+              ])
+            }
+          }
+          ])
         },
-        onEventResized: events => {
-          http.put(
-              `reservations/${events.source.data["id"]}`, {
-                space_id: this.space_id, 
-                user_id: 101, 
-                start_time: events.source.data["start"], 
-                end_time: events.source.data["end"]
-              })
-            location.reload()
-          console.log("Event resized")
+        onEventResize: () => {
+          console.log("event resized")
+          this.config.contextMenu = new DayPilot.Menu([
+          {
+            text: "Confirm resize",
+            onClick: events => {
+              try {
+                http.put(
+                  `reservations/${events.source.data["id"]}`, {
+                    space_id: this.space_id, 
+                    user_id: 101, 
+                    start_time: events.source.data["start"], 
+                    end_time: events.source.data["end"]
+                  }
+                )
+              } catch(error) {
+                console.error('Error resizing reservation:', error.message)
+              }
+              // reset context menu
+              this.config.contextMenu = new DayPilot.Menu([
+              {
+                text: "Show event ID",
+                onClick: events => {
+                  console.log(events.source.data)
+                }
+              },
+              {
+                text: "Delete",
+                onClick: events => {
+                  try {
+                    http.delete(
+                      `reservations/${events.source.data["id"]}`, {
+                        space_id: this.space_id, 
+                        user_id: 101, 
+                        start_time: events.source.data["start"], 
+                        end_time: events.source.data["end"]
+                      }
+                    )
+                    this.events = this.events.filter(event => event.id !== events.source.data["id"]);
+                    this.calendar.update({ events: this.events });
+                  } catch (error) {
+                    console.log("Error deleting reservation:", error.message)
+                  }
+                }
+              }
+              ])
+            }
+          }
+          ])
         },
       },
     }
@@ -121,19 +235,19 @@ export default {
   methods: {
     async loadEvents() {
       try {
-        const events = []
+        //const events = []
         const response = await http.get(`reservations/space/${this.space_id}`)
         console.log(this.user_id)
         
         response?.data?.forEach((item) => {
-          events.push({
+          this.events.push({
             id: item.id,
             start: item?.start_time,
             end: item?.end_time,
             text: "Booked"
           })
         })
-        this.calendar.update({events})
+        this.calendar.update({events: this.events})
       } catch (error) {
         console.error('Error loading events:', error)
       }
