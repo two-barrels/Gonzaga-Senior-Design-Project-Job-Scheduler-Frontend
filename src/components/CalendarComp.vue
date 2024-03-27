5ff48453198f9472e6ff69ff7da55309c4f7df46
lnguyen24 <lnguyen5670@gmail.com>
2024-03-27 14:28:10 -0700

    new edit feature, diff css for admin block

--- src/components/CalendarComp.vue
+++ src/components/CalendarComp.vue
@@ -2,7 +2,7 @@
   <div class="spaceName">
     <b>{{ space_name }}</b>
   </div>
-  <div class="wrap">
+  <div v-if="loaded" class="wrap">
     <div class="months">
       <DayPilotNavigator id="nav" :config="navigatorConfig" />
       <button type="button" @click=blockTimes>Block Times</button>
@@ -14,7 +14,7 @@
 </template>
 
 <script>
-import {DayPilot, DayPilotCalendar, DayPilotNavigator} from '@daypilot/daypilot-lite-vue'
+import { DayPilot, DayPilotCalendar, DayPilotNavigator } from '@daypilot/daypilot-lite-vue'
 import http from '@/services/http-helper.js'
 import date from '@/services/curr-date-helper.js'
 
@@ -23,6 +23,7 @@ export default {
   data() {
     return {
       events: [],
+      loaded: false,
       navigatorConfig: {
         showMonths: 2,
         skipMonths: 2,
@@ -33,181 +34,7 @@ export default {
           this.config.startDate = args.day
         }
       },
-      config: {
-        viewType: "Week",
-        contextMenu: new DayPilot.Menu([
-        {
-          text: "Show event ID",
-          onClick: events => {
-            console.log(events.source.data)
-          }
-        },
-        {
-          text: "Delete",
-          onClick: events => {
-            try {
-              http.delete(
-                `reservations/${events.source.data["id"]}`, {
-                  space_id: this.space_id, 
-                  user_id: 101, 
-                  start_time: events.source.data["start"], 
-                  end_time: events.source.data["end"]
-                }
-              )
-              this.events = this.events.filter(event => event.id !== events.source.data["id"]);
-              this.calendar.update({ events: this.events });
-            } catch (error) {
-              console.error("Error deleting reservation:", error.message)
-            }
-          }
-        }
-        ]),
-        startDate: date.currentDate(), // first week displayed
-        durationBarVisible: false,
-        timeRangeSelectedHandling: "Enabled",
-        onTimeRangeSelected: async (args) => {
-          const startTime = args.start.getTime()
-          const endTime = args.end.getTime()
-          if (this.overlapCheck(startTime, endTime)) {
-            args.preventDefault()
-            await DayPilot.Modal.alert("Error: Your reservation overlaps with an exisiting reservation")
-            return
-          }
-          else {
-            const resName = await DayPilot.Modal.prompt("Create a new reservation:", "Booked");
-            const calendar = args.control;
-            calendar.clearSelection();
-            if (resName.canceled) {
-              return
-            } 
-            calendar.events.add({
-              start: args.start,
-              end: args.end,
-              id: DayPilot.guid(),
-              text: resName.result
-            })
-            try {
-              await http.post(
-                'reservations', {
-                  space_id: this.space_id, 
-                  user_id: 101, 
-                  start_time: args.start, 
-                  end_time: args.end,
-                  text: resName.result
-                })
-            } catch (error) {
-              console.error('Error creating reservation:', error.message)
-            }
-          }
-        },
-        eventDeleteHandling: "Disabled",
-        eventRightClickHandling: "ContextMenu",
-        eventResizeHandling: "Update",
-        onEventMoved: () => {
-          console.log("Event moved")
-          this.config.contextMenu = new DayPilot.Menu([
-          {
-            text: "Confirm move",
-            onClick: events => {
-              try {
-                http.put(
-                  `reservations/${events.source.data["id"]}`, {
-                    space_id: this.space_id, 
-                    user_id: 101, 
-                    start_time: events.source.data["start"], 
-                    end_time: events.source.data["end"],
-                    text: events.source.data["text"]
-                  }
-                )
-              } catch (error) {
-                console.error("Error moving reservation:", error.message)
-              }
-              //reset context menu
-              this.config.contextMenu = new DayPilot.Menu([
-              {
-                text: "Show event ID",
-                onClick: events => {
-                  console.log(events.source.data)
-                }
-              },
-              {
-                text: "Delete",
-                onClick: events => {
-                  try {
-                    http.delete(
-                      `reservations/${events.source.data["id"]}`, {
-                        space_id: this.space_id, 
-                        user_id: 101, 
-                        start_time: events.source.data["start"], 
-                        end_time: events.source.data["end"],
-                        text: events.source.data["text"]
-                      }
-                    )
-                    this.events = this.events.filter(event => event.id !== events.source.data["id"]);
-                    this.calendar.update({ events: this.events });
-                  } catch (error) {
-                    console.error("Error deleting reservation:", error.message)
-                  }
-                }
-              }
-              ])
-            }
-          }
-          ])
-        },
-        onEventResize: () => {
-          console.log("event resized")
-          this.config.contextMenu = new DayPilot.Menu([
-          {
-            text: "Confirm resize",
-            onClick: events => {
-              try {
-                http.put(
-                  `reservations/${events.source.data["id"]}`, {
-                    space_id: this.space_id, 
-                    user_id: 101, 
-                    start_time: events.source.data["start"], 
-                    end_time: events.source.data["end"],
-                    text: events.source.data["text"]
-                  }
-                )
-              } catch(error) {
-                console.error('Error resizing reservation:', error.message)
-              }
-              // reset context menu
-              this.config.contextMenu = new DayPilot.Menu([
-              {
-                text: "Show event ID",
-                onClick: events => {
-                  console.log(events.source.data)
-                }
-              },
-              {
-                text: "Delete",
-                onClick: events => {
-                  try {
-                    http.delete(
-                      `reservations/${events.source.data["id"]}`, {
-                        space_id: this.space_id, 
-                        user_id: 101, 
-                        start_time: events.source.data["start"], 
-                        end_time: events.source.data["end"],
-                        text: events.source.data["text"]
-                      }
-                    )
-                    this.events = this.events.filter(event => event.id !== events.source.data["id"]);
-                    this.calendar.update({ events: this.events });
-                  } catch (error) {
-                    console.error("Error deleting reservation:", error.message)
-                  }
-                }
-              }
-              ])
-            }
-          }
-          ])
-        },
-      },
+      config: this.initCalendar()
     }
   },
   props: {
@@ -230,18 +57,27 @@ export default {
         const response = await http.get(`reservations/space/${this.space_id}`)
         
         response?.data?.forEach((item) => {
+          console.log(item.admin_block)
           this.events.push({
             id: item.id,
             start: item?.start_time,
             end: item?.end_time,
-            text: item?.text
+            text: item?.text,
+            backColor: item.admin_block ? "#FF9700" : undefined
           })
         })
-        this.calendar.update({events: this.events})
+        this.calendar.update({ events: this.events })
       } catch (error) {
         console.error('Error loading events:', error)
       }
     },
+    // adjustToLocal(time) {
+    //   return new Date(time.getTime() + time.getTimezoneOffset() * 60000).toISOString().slice(0, 19)
+    // },
+    // adjustToUtc(time) {
+    //   console.log(time.toISOString().slice(0, 19))
+    //   return time.toISOString().slice(0, 19)
+    // },
     async blockTimes() {
       const form = [
         {name: "Start Date/Time", id: "start", dateFormat: "M/d/yyyy", timeInterval: 30, type: "datetime"},
@@ -253,7 +89,8 @@ export default {
         start: blockRange.result["start"]["value"],
         end: blockRange.result["end"]["value"],
         id: DayPilot.guid(),
-        text: "Blocked off by admin"
+        text: "Blocked off by admin",
+        backColor: "#FF9700"
       })
       try {
         await http.post(
@@ -262,13 +99,15 @@ export default {
             user_id: 101, 
             start_time: blockRange.result["start"]["value"], 
             end_time: blockRange.result["end"]["value"],
-            text: "Blocked off by admin"
+            text: "Blocked off by admin",
+            admin_block: true
           })
       } catch (error) {
         console.error('Error creating reservation:', error.message)
       }
       this.calendar.update({events: this.events})
     },
+
     overlapCheck(start, end) {
       return this.events.some((item) => {
         const existingStart = item.start.getTime()
@@ -276,9 +115,137 @@ export default {
         return (start >= existingStart && start <= existingEnd) ||
             (end >= existingStart && end <= existingEnd)
       })
+    },
+
+    initCalendar() {
+      return {
+        viewType: "Week",
+        contextMenu: this.initContextMenu(),
+        startDate: date.currentDate(), // first week displayed
+        durationBarVisible: false,
+        timeRangeSelectedHandling: "Enabled",
+        onTimeRangeSelected: args => {
+          this.handleTimeRangeSelected(args)
+        },
+        eventDeleteHandling: "Disabled",
+        eventRightClickHandling: "ContextMenu",
+        eventResizeHandling: "Disabled",
+        eventMoveHandling: "Disabled",
+      }
+    },
+
+    initContextMenu() {
+      return new DayPilot.Menu([
+        // dev purposes
+        // {
+        //   text: "Show event ID",
+        //   onClick: events => {
+        //     console.log(events.source.data["id"])
+        //   }
+        // },
+        {
+          text: "Delete",
+          onClick: events => {
+            this.handleDelete(events)
+          }
+        },
+        {
+          text: "Edit",
+          onClick: events => {
+            this.handleEditEvent(events)
+          }
+        }
+      ])
+    },
+
+    handleDelete(events) {
+      try {
+        http.delete(
+          `reservations/${events.source.data["id"]}`, {
+            space_id: this.space_id, 
+            user_id: 101, 
+            start_time: events.source.data["start"], 
+            end_time: events.source.data["end"]
+          }
+        )
+        this.events = this.events.filter(event => event.id !== events.source.data["id"]);
+        this.calendar.update({ events: this.events });
+      } catch (error) {
+        console.error("Error deleting reservation:", error.message)
+      }
+    },
+
+    async handleTimeRangeSelected(args) {
+      const startTime = args.start.getTime()
+      const endTime = args.end.getTime()
+      if (this.overlapCheck(startTime, endTime)) {
+        args.preventDefault()
+        await DayPilot.Modal.alert("Error: Your reservation overlaps with an exisiting reservation")
+        return
+      }
+      else {
+        const resName = await DayPilot.Modal.prompt("Create a new reservation:", "Booked");
+        const calendar = args.control;
+        calendar.clearSelection();
+        if (resName.canceled) {
+          return
+        } 
+        calendar.events.add({
+          start: args.start,
+          end: args.end,
+          id: DayPilot.guid(),
+          text: resName.result
+        })
+        try {
+          await http.post(
+            'reservations', {
+              space_id: this.space_id, 
+              user_id: 101, 
+              start_time: args.start, 
+              end_time: args.end,
+              text: resName.result,
+              admin_block: false
+            })
+        } catch (error) {
+          console.error('Error creating reservation:', error.message)
+        }
+      }
+    },
+
+    async handleEditEvent(events) {
+      const form = [
+        {name: "Start Date/Time", id: "start", dateFormat: "M/d/yyyy", timeInterval: 30, type: "datetime"},
+        {name: "End Date/Time", id: "end", dateFormat: "M/d/yyyy", timeInterval: 30, type: "datetime"}
+      ]
+      const blockRange = await DayPilot.Modal.form(form)
+
+      this.events = this.events.filter(event => event.id !== events.source.data["id"]);
+      this.events.push({
+        start: blockRange.result["start"]["value"],
+        end: blockRange.result["end"]["value"],
+        id: DayPilot.guid(),
+        text: events.source.data["text"]
+      })
+
+      try {
+        await http.put(
+          `reservations/${events.source.data["id"]}`, {
+            space_id: this.space_id, 
+            user_id: 101, 
+            start_time: blockRange.result["start"]["value"], 
+            end_time: blockRange.result["end"]["value"],
+            text: events.source.data["text"],
+            admin_block: false
+          })
+      } catch (error) {
+        console.error('Error creating reservation:', error.message)
+      }
+      this.calendar.update({events: this.events})
     }
   },
   async mounted() {
+    this.config = this.initCalendar()
+    this.loaded = true
     await this.loadEvents()
   }
 }
@@ -302,7 +269,6 @@ export default {
 
 .calendar_default_event_inner {
   background: #f16d01;
-  //color: white;
   color: red;
   border-radius: 5px;
   opacity: 0.9;
