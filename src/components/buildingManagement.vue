e590cd0391a75778f5c5688d3df7b44e0aaa600e
EFigueroa12 <efigueroa6912@gmail.com>
2024-04-17 12:07:05 -0700

    added buildingManagement Component

--- src/components/buildingManagement.vue
+++ src/components/buildingManagement.vue
@@ -1,36 +1,36 @@
 <template>
     <div class="row">
         <div class="col-4">
-          <div 
-            class="list-group overflow-auto" 
-            style="max-height: 100vh;" 
-            id="list-tab" 
-            role="tablist"
-          >
-            <a 
-              v-for="(building, idx) in buildings" 
-              :key="idx" 
-              class="list-group-item list-group-item-action" 
-              :id="'list-' + building.name" 
-              data-bs-toggle="list" 
-              role="tab" 
-              :aria-controls="'list-' + building.name" 
-              :href="'#' + building.name">{{ building.name }}
-            </a>
+            <div 
+                class="list-group overflow-auto" 
+                style="max-height: 100vh;" 
+                id="list-tab" 
+                role="tablist"
+            >
+                <a 
+                    v-for="(building, idx) in buildings" 
+                    :key="idx" 
+                    class="list-group-item list-group-item-action" 
+                    :id="'list-' + building.name" 
+                    data-bs-toggle="list" 
+                    role="tab" 
+                    :aria-controls="'list-' + building.name" 
+                    :href="'#' + building.name">{{ building.name }}
+                </a>
           </div>
         </div>
         <div class="col-8">
-          <div class="tab-content tab-details" id="nav-tabContent">
-            <div 
-              v-for="(building, idx) in buildings" 
-              :key="idx" class="tab-pane fade" 
-              :id="building.name" 
-              role="tabpanel" 
-              :aria-labelledby="'list-' + building.name"
-            >
-              <floor-space building="building" />
+            <div class="tab-content tab-details" id="nav-tabContent">
+                <div 
+                    v-for="(building, idx) in buildings" 
+                    :key="idx" class="tab-pane fade" 
+                    :id="building.name" 
+                    role="tabpanel" 
+                    :aria-labelledby="'list-' + building.name"
+                >
+                    <floor-space :building="building" />
+                </div>
             </div>
-          </div>
         </div>
       </div>
     </template>
